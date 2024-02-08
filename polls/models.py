3099b22d239c47f3a67b8ccc9f003dca59ed7be2
Douglas Martinez <douggmartinez12@gmail.com>
2024-02-07 21:26:50 -0600

    chre: adding requiemenets.txt

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.utils import timezone
-
 import datetime
 
 
@@ -15,3 +14,4 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
